8b6a0dbdb636ffac7f1aa486ad3fd0f3f5b6a82f
Liam S <37228196+LiamStachiw@users.noreply.github.com>
2023-08-08 22:34:00 -0400

    Basic setup/configuration

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Example
-An example greeter plugin
+# Slayer Streak Reminder
+This is a plugin designed to remind the player to get their next slayer task from the best available slayer master when their next slayer task will give a points bonus.
+
+The user will be able to select which slayer master will be suggested in a dropdown menu in the settings. This will default to Konar quo Maten.
--- src/test/java/com/slayerstreakreminder/SlayerStreakReminderPluginTest.java
+++ src/test/java/com/slayerstreakreminder/SlayerStreakReminderPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.slayerstreakreminder;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class SlayerStreakReminderPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(SlayerStreakReminderPlugin.class);
 		RuneLite.main(args);
 	}
 }
